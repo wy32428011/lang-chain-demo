462390ee02d267dccfa3817a2c4af43e02d741b6
wangyun <wangyun@zdjc.cn>
2025-08-07 15:42:57 +0800

    add：结构修改

---
+++ demo/setting.py
@@ -0,0 +1,9 @@
+CONFIG_SETTING = {
+    "llm": {
+        "base_url": "",
+        "api_key": "",
+        "model_name": "gpt-3.5-turbo",
+        "temperature": 0.3,
+        "max_tokens": 20480,
+    },
+}
